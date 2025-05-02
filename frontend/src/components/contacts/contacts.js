16b6c5e15c875f15d1605c0a8bbe0e56945f2ea1
walid-zouguagh <walid.zouguaagh@gmail.com>
2025-05-02 19:46:34 +0100

    chatbox

---
+++ frontend/src/components/contacts/contacts.js
@@ -0,0 +1,6 @@
+const contact = document.querySelector('.container');
+const chatBox = document.querySelector('.chatBox');
+
+contact.addEventListener('click', ()=>{
+    chatBox.style.display = 'block'
+});
