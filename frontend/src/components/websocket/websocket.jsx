4753acfc52aa1f43d299ecfd4cbdb6a9c3211a7d
walid-zouguaagh <walid.zouguaagh@gmail.com>
2025-05-04 02:24:17 +0100

    create component websocket

---
+++ frontend/src/components/websocket/websocket.jsx
@@ -0,0 +1,16 @@
+
+export default function Websocket() {
+    // Create WebSocket connection.
+    const socket = new WebSocket("ws://localhost:8080/ws");
+
+    // Connection opened
+    socket.addEventListener("open", (event) => {
+        socket.send("Hello Server!");
+    });
+
+    // Listen for messages
+    socket.addEventListener("message", (event) => {
+        console.log("Message from server ", event.data);
+    });
+
+}
