cabd390f45a519bccfe0cd922debbbc5b46d61cd
ayoub-draoui <mr.stefan.draoui@gmail.com>
2025-05-08 17:06:53 +0100

    deleting front

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
@@ -13,11 +14,11 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+
 func main() {
-	mux := http.NewServeMux()
-	
-	http.ListenAndServe(":8080", mux)
-	mux.Handle("/login", midleware.WithCORS(http.HandlerFunc(controllers.Login)))
+mux := http.NewServeMux()
+
+	mux.Handle("/login",midleware.WithCORS(http.HandlerFunc(controllers.Login)))
 	mux.Handle("/register", midleware.WithCORS(http.HandlerFunc(controllers.Register)))
 	mux.Handle("POST /addPost", midleware.WithCORS(http.HandlerFunc(controllers.AddPost)))
 	mux.Handle("POST /followReq", midleware.WithCORS(http.HandlerFunc(controllers.HandleFollow)))
@@ -25,15 +26,21 @@ func main() {
 	mux.Handle("POST /creategroup", midleware.WithCORS(http.HandlerFunc(controllers.CreateGroup)))
 	mux.Handle("POST /joinReq", midleware.WithCORS(http.HandlerFunc(controllers.JoinReq)))
 	mux.Handle("POST /api/logout", midleware.WithCORS(http.HandlerFunc(controllers.LogoutHandler)))
-	mux.Handle("POST /api/reactions", midleware.WithCORS(http.HandlerFunc(controllers.HandelReactions)))
+
+	
 	models.Db = db.InitDB()
 	defer models.Db.Close()
 	mux.HandleFunc("GET /uploads/", controllers.HandelPics)
-	mux.HandleFunc("GET /api/getProfilePosts", controllers.GetProfilePosts)
+	mux.HandleFunc("GET /api/getProfilePosts",controllers.GetProfilePosts)
 	mux.HandleFunc("GET /api/posts", controllers.Posts)
 	mux.HandleFunc("GET /group/{GroupName}", controllers.Group)
-	mux.HandleFunc("GET /api/getfollowers", controllers.GetFollowers)
-	mux.HandleFunc("GET /api/registrationData", controllers.GetRegistrationData)
-	mux.HandleFunc("/ws", controllers.Websocket)
-
+	mux.HandleFunc("GET /api/getfollowers",controllers.GetFollowers)
+	mux.HandleFunc("GET /api/registrationData",controllers.GetRegistrationData)
+    mux.HandleFunc("/ws", controllers.Websocket)
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", mux)
 }
+
+
+
+
