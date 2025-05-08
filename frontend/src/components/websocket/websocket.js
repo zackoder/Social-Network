1b90408f9430818997ff9d03001c63da4d773d0f
walid-zouguagh <walid.zouguaagh@gmail.com>
2025-05-08 17:14:49 +0100

    websocket

--- frontend/src/components/websocket/websocket.js
+++ frontend/src/components/websocket/websocket.js
@@ -1,33 +1,52 @@
 export let socket;
+export let websocketConnection = null;
 
 export function Websocket() {
-    // if (!socket || socket.readyState === WebSocket.CLOSED) {
-
+    
+    if (websocketConnection) {
+        return websocketConnection;
+    }
+    websocketConnection = new Promise((resolve, reject) => {   
         // Create WebSocket connection.
-        socket = new WebSocket("ws://localhost:8080/ws")
+        socket = new WebSocket("ws://localhost:8080/ws");
 
         // Connection opened
         socket.addEventListener("open", (event) => {
             // socket.send("Hello Server!");
             console.log("Websocket Connected");
+            resolve(socket);
             
         });
 
-        // Listen for messages
-        socket.addEventListener("message", (event) => {
-            console.log("Message from server ", event.data);
-        });
-    
         //Websocket Closed
-        socket.addEventListener('close', () => {
+        socket.addEventListener('close', (event) => {
             console.log("WebSocket closed");
+            websocketConnection = null;
+            reject(new Error('websocket connection closed'));
         });
 
         //Websocket error
         socket.addEventListener('error', (err) => {
             console.log("WebSocket error", err);
+            reject(err);
         });
 
-    // }
+        // Listen for messages
+        socket.addEventListener("message", (event) => {
+            console.log("Message from server ", event.data);
+        });        
+    });
+    return websocketConnection;
 
 }
+
+
+// type Message struct {
+// 	Sender_id   int    `json:"sender_id"`
+// 	Reciever_id int    `json:"reciever_id"`
+// 	Type        string `json:"type"`
+// 	Group_id    int    `json:"group_id"`
+// 	Content     string `json:"content"`
+// 	Mime        string `json:"mime"`
+// 	Filename    string `json:"filename"`
+// }
