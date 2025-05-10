4568fcf1ecc6c5f31c20de0fdc405fae9f7605e8
Your Name <you@example.com>
2025-05-10 16:32:05 +0100

    jj

--- frontend/src/components/footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./footer.module.css"
-export default function Footer(){
-    return(
-        // <div className=>© Copywrite</div>
-        <div className={styles.container}>
-            <div>© 2024 Social-Network. All rights reserved.</div>
-        </div>
-    )
-}
