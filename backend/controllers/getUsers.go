a82e17886c6cb4e21609449389a809dde46090b8
ayoub-draoui <mr.stefan.draoui@gmail.com>
2025-05-18 00:03:55 +0100

    ff

--- backend/controllers/getUsers.go
+++ backend/controllers/getUsers.go
@@ -9,22 +9,17 @@ import (
 )
 
 func GetUsers(w http.ResponseWriter, r *http.Request,userID int) {
-	// cookie, err := r.Cookie("token")
-	// if err != nil {
-	// 	utils.WriteJSON(w, map[string]string{"error": "Unauthorized"}, http.StatusUnauthorized)
-	// 	return
-	// }
-	// userID, err := models.Get_session(cookie.Value)
-	// if err != nil {
-	// 	utils.WriteJSON(w, map[string]string{"error": "Session not found"}, http.StatusUnauthorized)
-	// 	return
-	// }
+	if r.Method != "GET" {
+		utils.WriteJSON(w, map[string]string{"error":"Method Not Allowd"}, http.StatusMethodNotAllowed)
+	}
+	fmt.Println("im used")
+	fmt.Println(userID)
+	// userID = 11
 	users, err := models.GetUserFriends(userID)
 	if err != nil {
-		fmt.Println("can't fetch users")
+		utils.WriteJSON(w,map[string]string{"error":"Internal Server Error"},http.StatusInternalServerError)
+		fmt.Println("can't fetch users from DataBase")
 		return
 	}
 	utils.WriteJSON(w, users,http.StatusAccepted)
 }
-
-
