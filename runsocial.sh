b22fe32494741b356e90d570c644b3578dedacb0
zackoder <besszakaria@gmail.com>
2025-05-24 10:44:47 +0100

    bash screpte for running the program is added

---
+++ runsocial.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ ! -e "./frontend/.env" ]; then 
+    echo "NEXT_PUBLIC_HOST=http://localhoset:8080" >> .env
+fi
+
+# cd frontend 
+# npm i
+
+# cd ../backend
+# export PATH="$PATH:$HOME/go/bin"
+# source ~/.bashrc
+# fresh -g
+# fresh
+
+# Start frontend in a new terminal
+gnome-terminal -- bash -c "
+cd frontend
+npm install
+npm run dev
+exec bash
+"
+
+# Start backend in another new terminal
+gnome-terminal -- bash -c "
+cd backend
+export PATH=\"\$PATH:\$HOME/go/bin\"
+source ~/.bashrc
+fresh -g
+fresh
+exec bash
+"
