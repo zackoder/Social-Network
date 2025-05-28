cc04f2c832ef122e2ca937740cbcbdc8defeca39
ayoub-draoui <mr.stefan.draoui@gmail.com>
2025-05-28 20:47:25 +0100

    fixing pp && starting comments

--- backend/controllers/getFollowers.go
+++ backend/controllers/getFollowers.go
@@ -14,29 +14,40 @@ func GetFollowers(w http.ResponseWriter, r *http.Request, userID int) {
 		utils.WriteJSON(w, map[string]string{"error": "Method Not Allowed"}, http.StatusMethodNotAllowed)
 		return
 	}
-	userId, err := strconv.Atoi(r.URL.Query().Get("id"))
+	userId, err := strconv.Atoi((r.URL.Query().Get("id")))
+	fmt.Println(userID, userId)
 	if err != nil {
 		fmt.Println("we cant convert the id")
 		utils.WriteJSON(w, map[string]string{"error": "data not available "}, http.StatusBadRequest)
 		return
 	}
-	followers, err := models.GetFollowers(userId)
-	fmt.Println(followers)
-	if err != nil {
-		utils.WriteJSON(w, map[string]string{"error": "We Can't Fetsh Followers List"}, http.StatusInternalServerError)
-		return
+	if userId == userID {
+		MyFollowers, err := models.GetFollowers(userID)
+		fmt.Println(MyFollowers)
+		if err != nil {
+			utils.WriteJSON(w, map[string]string{"error": "We Can't Fetsh Followers List For Posts"}, http.StatusInternalServerError)
+			return
+		}
+		utils.WriteJSON(w, MyFollowers, http.StatusAccepted)
+
+	} else {
+		UserFollowers, err := models.GetFollowers(userId)
+		if err != nil {
+			utils.WriteJSON(w, map[string]string{"error": "We Can't Fetsh Followers List For Profile"}, http.StatusInternalServerError)
+			return
+		}
+		utils.WriteJSON(w, UserFollowers, http.StatusAccepted)
 	}
-	utils.WriteJSON(w, followers, http.StatusAccepted)
 }
 
-func Getfollowings(w http.ResponseWriter, r *http.Request) {
+func GetfollowingsForProfile(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		utils.WriteJSON(w, map[string]string{"error": "Method Not Allowed"}, http.StatusMethodNotAllowed)
 		return
 	}
 	userId, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil {
-		fmt.Println("we cant convert the id")
+		fmt.Println("111111we cant convert the id")
 		utils.WriteJSON(w, map[string]string{"error": "data not available "}, http.StatusBadRequest)
 		return
 	}
